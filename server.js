25f2a9cfbdcde9b821eb17513cc6a654bef5c5f8
victorio222 <cabatinganvictorio@gmail.com>
2025-06-15 18:03:27 +0800

    server

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+const PORT = process.env.PORT || 5000;
+const allowedOrigins = ['http://localhost:3000'];
+
+app.use(express.json());
+
+app.use(cors({
+    origin: function(origin, cb) {
+        if(origin) return cb(null, true);
+        if(allowedOrigins.includes(origin)) {
+            return cb(null, true);
+        } else {
+            return cb("Not allowed by cors");
+        }
+    },
+    credentials: true
+}));
+
+// routes
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http0://localhost:${PORT}`);
+})
