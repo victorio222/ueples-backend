68ab5249f4d6abf6240b17b9b9246e0dd2ae32e2
victorio222 <cabatinganvictorio@gmail.com>
2025-07-09 22:01:00 +0800

    logsupdate

--- src/routers/logs.router.js
+++ src/routers/logs.router.js
@@ -3,7 +3,7 @@ import logsController from "../controllers/logs.controller.js";
 
 const logRouter = express.Router();
 
-logRouter.post("/", logsController.createLog);
+logRouter.post("/add", logsController.createLog);
 logRouter.get("/", logsController.getAllLogs);
 logRouter.get("/filter", logsController.getLogsByFilter);
 logRouter.get("/user/:user_id", logsController.getLogsByUser);
