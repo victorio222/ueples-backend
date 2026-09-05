2a51db81f94e33181766a9bcaba0eb0168c5954e
cabatinganvictorio <cabatinganvictorio@gmail.com>
2026-09-05 14:46:35 +0800

    fix

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -8,10 +8,11 @@ import authServices from "../services/auth.services.js";
  */
 const cookieOptions = {
     httpOnly: true,
-    secure: true,
-    sameSite: 'none',
-    // secure: false,
-    // sameSite: 'Lax',
+    // secure: true,
+    // sameSite: 'none',
+    secure: false,
+    sameSite: 'Lax',
+    maxAge: 7 * 24 * 60 * 60 * 1000,
     path: '/',
 };
 
--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -36,10 +36,11 @@ async function authToken(req, res, next) {
                 res.cookie('token', newAccessToken, { 
                     httpOnly: true, 
                     // secure: process.env.NODE_ENV === 'production', // Use secure in prod
-                    secure: true,
-                    sameSite: 'none',
-                    // secure: false,
-                    // sameSite: 'Lax',
+                    // secure: true,
+                    // sameSite: 'none',
+                    secure: false,
+                    sameSite: 'Lax',
+                    maxAge: 7 * 24 * 60 * 60 * 1000,
                     path: '/'
                 });
                 
